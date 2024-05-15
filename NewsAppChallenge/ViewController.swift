532ccc3c7c8bc93a6797616abbd8e0040d8edb6d
filipeilunga <filipeilunga@gmail.com>
2024-05-14 23:52:37 -0300

    Initial Commit

---
+++ NewsAppChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppChallenge
+//
+//  Created by Filipe Ilunga on 14/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
