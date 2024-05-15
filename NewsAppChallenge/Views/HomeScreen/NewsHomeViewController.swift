22804351ff6a088fdb6c08a909e55c0bd9df5cbc
filipeilunga <filipeilunga@gmail.com>
2024-05-15 00:19:11 -0300

    refactor: Add NewsHomeViewController

--- NewsAppChallenge/Views/HomeScreen/NewsHomeViewController.swift
+++ NewsAppChallenge/Views/HomeScreen/NewsHomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NewsHomeViewController.swift
 //  NewsAppChallenge
 //
 //  Created by Filipe Ilunga on 14/05/24.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsHomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
 
 }
 
